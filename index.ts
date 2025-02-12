e6d94e0d53ab80ab2f4d32a8a2cf01fb44f30d04
asad-ali-developer <uhope1645@gmail.com>
2025-02-12 22:54:21 +0500

    Improved Folder Structure

--- index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./src/decorators/ClassDecorator";
-export * from "./src/decorators/MethodDecorator";
-export * from "./src/decorators/Middlewares";
-export * from "./src/types";
-export { registerControllers, setApiGlobalPrefix } from "./src/registerController";
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from "./decorators/ClassDecorator";
+export * from "./decorators/MethodDecorator";
+export * from "./decorators/Middlewares";
+export * from "./types";
+export { registerControllers, setApiGlobalPrefix } from "./registerController";
