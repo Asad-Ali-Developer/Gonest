bcfd42c6a7756dea1310943a32e8fe0c03573c61
asad-ali-developer <uhope1645@gmail.com>
2025-02-21 23:06:14 +0500

    Pushed preivous code

--- src/scripts/add-dev-script.js
+++ src/scripts/add-dev-script.js
@@ -10,7 +10,7 @@ const isTypeScript = fs.existsSync(tsConfigPath);
 
 const devScript = isTypeScript
   ? "ts-node-dev --clear --respawn --transpile-only --watch src src/index.ts"
-  : "nodemon --exec \"node scripts/cls.js && node index.js\" --watch .";
+  : "nodemon --exec \"cls && node index.js\" --watch .";
 
 function updatePackageJson() {
   if (!fs.existsSync(packageJsonPath)) {
@@ -26,24 +26,4 @@ function updatePackageJson() {
   // console.log("'dev' script set to:", devScript);
 }
 
-
-// Copy clear-terminal.js to user’s project if not present
-function copyClearTerminal() {
-
-  // Path to the clear-terminal.js in your gonest package
-  const sourcePath = path.resolve(__dirname, "cls.js");
-  
-  // Destination in the user’s project
-  const destDir = path.resolve(process.cwd(), "scripts");
-  const destPath = path.join(destDir, "cls.js");
-
-  fs.mkdirSync(destDir, { recursive: true });
-
-  if (!fs.existsSync(destPath)) {
-    fs.copyFileSync(sourcePath, destPath);
-  }
-}
-
 updatePackageJson();
-// Only copy the file if we’re dealing with JS (not TS) or you can always copy
-copyClearTerminal();
